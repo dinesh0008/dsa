6bf806ab1ac53df23cfeaca789508d8aa09084c0
dinesh-ADT <dinesh.k@altradimension.com>
2020-01-29 17:14:08 +0530

    linked list added

---
+++ LinkedList/reverse.py
@@ -0,0 +1,45 @@
+class Node:
+    
+    def __init__(self, data):
+
+        self.data = data
+        self.next = None
+
+class LinkedList:
+
+    def __init__(self):
+
+        self.head = None
+
+    def push(self,data):
+
+        node = Node(data)
+        node.next = self.head
+        self.head  = node
+
+    def reverse(self,node):
+
+        if node.next is None:
+            self.head = node
+            node.next = None
+            return
+
+        self.reverse(node.next)
+        node.next.next = node
+        node.next = None
+
+        
+
+
+        
+
+if __name__ == '__main__':
+
+    ll = LinkedList()
+    ll.push(1)
+    ll.push(2)
+    ll.push(3)
+    ll.push(4)
+    print(ll.head.data)
+    ll.reverse(ll.head)
+    print(ll.head.data)
